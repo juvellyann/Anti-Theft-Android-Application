3b80a3a7012b5774ca4289660e09481105635540
Juvelly Ann Aguilar <124800035+juvellyann@users.noreply.github.com>
2023-07-08 21:22:58 +0800

    add single image

--- app/src/main/java/com/example/myapplication/GalleryFragment.java
+++ app/src/main/java/com/example/myapplication/GalleryFragment.java
@@ -1,12 +1,33 @@
 package com.example.myapplication;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 ///**
 // * A simple {@link Fragment} subclass.
@@ -14,7 +35,8 @@ import android.view.ViewGroup;
 // * create an instance of this fragment.
 // */
 public class GalleryFragment extends Fragment {
-
+    List<String> fileName = new ArrayList<>();
+    ImageView imageView;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -49,6 +71,7 @@ public class GalleryFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        new HttpRequestTask().execute();
 //        if (getArguments() != null) {
 //            mParam1 = getArguments().getString(ARG_PARAM1);
 //            mParam2 = getArguments().getString(ARG_PARAM2);
@@ -60,5 +83,95 @@ public class GalleryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_gallery, container, false);
+    }
+
+    private void displayImages() {
+
+    }
+    class HttpRequestTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+
+                // Create a URL object with the endpoint you want to make the request to
+                URL url = new URL("http://api.imbento.com/others/ctu2023_motorcycle_anti_theft/db.php?action=imgs&did=1");
+
+                // Create an HttpURLConnection object
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                // Set the request method (GET, POST, etc.)
+                connection.setRequestMethod("GET");
+
+                // Get the response code
+                int responseCode = connection.getResponseCode();
+
+                // Read the response
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                // Disconnect the connection
+                connection.disconnect();
+
+                // Return the response data
+                return response.toString();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String responseData) {
+            super.onPostExecute(responseData);
+
+            if (responseData != null) {
+                Log.d("Gallery", responseData);
+                // Process the response data as needed
+                JSONArray jsonArray = null;
+                try {
+                    jsonArray = new JSONArray(responseData);
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                        String id = jsonObject.getString("id");
+                        String deviceId = jsonObject.getString("device_id");
+                        String filename = jsonObject.getString("sFilename");
+                        String dateTime = jsonObject.getString("dDateTime");
+
+                        // Do something with the parsed data
+                        // For example, you can create objects or store the values in a list
+                        // or use them as needed in your application
+
+                        fileName.add(filename);
+                        Log.d("sFilename: " , fileName.toString());
+
+                    }
+
+                    imageView  = getView().findViewById(R.id.img1);
+                    Picasso.get()
+                            .load("http://api.imbento.com/others/ctu2023_motorcycle_anti_theft/uploads/"+fileName.get(0))
+                            .into(imageView);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                // Update the UI if necessary
+                // getActivity().runOnUiThread(new Runnable() {
+                //     @Override
+                //     public void run() {
+                //         // Update the UI with the response data
+                //     }
+                // });
+            } else {
+                // Handle the case when the request fails
+            }
+        }
     }
 }
