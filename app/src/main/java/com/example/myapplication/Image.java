74daaf8dccdc91b36c0039f97ec5827b360117a0
Juvelly Ann Aguilar <124800035+juvellyann@users.noreply.github.com>
2023-07-09 12:11:50 +0800

    gallery method ok!
    gallery method ok!

---
+++ app/src/main/java/com/example/myapplication/Image.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+public class Image {
+    String id;
+
+    public Image(String id, String img, String dateTime) {
+        this.id = id;
+        this.img = img;
+        this.dateTime = dateTime;
+    }
+
+    String img;
+    String dateTime;
+}
