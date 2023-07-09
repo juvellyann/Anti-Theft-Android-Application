c999157af3f299f1b3a3988db98600525655f91e
Juvelly Ann Aguilar <124800035+juvellyann@users.noreply.github.com>
2023-07-09 12:04:46 +0800

    gallery method ok!

--- app/src/main/java/com/example/myapplication/GalleryFragment.java
+++ app/src/main/java/com/example/myapplication/GalleryFragment.java
@@ -9,9 +9,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import com.squareup.picasso.Picasso;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -35,8 +39,10 @@ import java.util.List;
 // * create an instance of this fragment.
 // */
 public class GalleryFragment extends Fragment {
-    List<String> fileName = new ArrayList<>();
+    ArrayList<Image> fileName = new ArrayList<>();
+    ArrayList<Image> sfileName = new ArrayList<>();
     ImageView imageView;
+    GalleryAdapter gadapter;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -72,6 +78,7 @@ public class GalleryFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         new HttpRequestTask().execute();
+
 //        if (getArguments() != null) {
 //            mParam1 = getArguments().getString(ARG_PARAM1);
 //            mParam2 = getArguments().getString(ARG_PARAM2);
@@ -82,7 +89,76 @@ public class GalleryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        Spinner monthSpinner = rootView.findViewById(R.id.monthSpinner);
+
+        // Create an ArrayAdapter with month items
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, getMonthItems());
+
+        // Set the dropdown layout style
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Set the adapter to the spinner
+        monthSpinner.setAdapter(adapter);
+        gadapter = new GalleryAdapter(getActivity(), sfileName);
+        ListView listView = rootView.findViewById(R.id.listView);
+        listView.setAdapter(gadapter);
+
+        // Set a listener for item selection
+        monthSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedMonth = parent.getItemAtPosition(position).toString();
+                // Do something with the selected month
+                sfileName.clear();
+                if(position == 0){
+                    sfileName.addAll(fileName);
+                }else{
+                    int i = 0;
+                    for(;i < fileName.size();i++){
+                        String[] tokens = fileName.get(i).dateTime.split("-");
+                        try{
+                            int number = Integer.parseInt(tokens[1]);
+                            if(number == position){
+                                sfileName.add(fileName.get(i));
+                            }
+                        }
+                        catch (NumberFormatException ex){
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+
+
+                gadapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Handle the case when nothing is selected
+            }
+        });
+        return rootView;
+    }
+
+    private List<String> getMonthItems() {
+        List<String> months = new ArrayList<>();
+        months.add("Month");
+        months.add("January");
+        months.add("February");
+        months.add("March");
+        months.add("April");
+        months.add("May");
+        months.add("June");
+        months.add("July");
+        months.add("August");
+        months.add("September");
+        months.add("October");
+        months.add("November");
+        months.add("December");
+        return months;
     }
 
     private void displayImages() {
@@ -135,6 +211,7 @@ public class GalleryFragment extends Fragment {
                 Log.d("Gallery", responseData);
                 // Process the response data as needed
                 JSONArray jsonArray = null;
+                sfileName.clear();
                 try {
                     jsonArray = new JSONArray(responseData);
                     for (int i = 0; i < jsonArray.length(); i++) {
@@ -148,27 +225,18 @@ public class GalleryFragment extends Fragment {
                         // Do something with the parsed data
                         // For example, you can create objects or store the values in a list
                         // or use them as needed in your application
-
-                        fileName.add(filename);
-                        Log.d("sFilename: " , fileName.toString());
-
+                        Log.d("Image Id",id+"");
+                        fileName.add(new Image(id,filename,dateTime));
                     }
+                    sfileName.addAll(fileName);
+                    gadapter.notifyDataSetChanged();
 
-                    imageView  = getView().findViewById(R.id.img1);
-                    Picasso.get()
-                            .load("http://api.imbento.com/others/ctu2023_motorcycle_anti_theft/uploads/"+fileName.get(0))
-                            .into(imageView);
+
                 } catch (JSONException e) {
                     throw new RuntimeException(e);
                 }
 
-                // Update the UI if necessary
-                // getActivity().runOnUiThread(new Runnable() {
-                //     @Override
-                //     public void run() {
-                //         // Update the UI with the response data
-                //     }
-                // });
+
             } else {
                 // Handle the case when the request fails
             }
